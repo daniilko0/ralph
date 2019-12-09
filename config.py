0abe23f4ac24588c9a3254e3352d527e543227e4
Daniil <dadyarri@gmail.com>
2019-12-09 11:00:17 +0300

    Update .gitignore; remove compromised files.

--- config.py
+++
@@ -1,7 +0,0 @@
-auth = {
-    'token': '6707e6009d9b09fcd6179cacbe30780f686f54b258476304eae95ef5ab311b31180cd9157c00bebc27717',
-    'cid': 2000000002,
-    'gid': 186618873,
-    'table': '1dzTEVaf0xsd_oGHyB0UZnL64s85I-6Qvqaq8CzdNEwk',
-    'admins': [549350532, 217248015, 332479032],
-}

5228be015fdbf86fafc1dc9985d34b5a305aa57b
Daniil <dadyarri@gmail.com>
2020-01-10 16:48:44 +0300

    Add class Schedule (get current status of schedule).

--- schedule.py
+++ schedule.py
@@ -13,3 +13,23 @@ class Date:
     @property
     def tomorrow(self):
         return datetime.strftime(datetime.now() + timedelta(1), "%Y-%m-%d")
+
+
+class Schedule:
+    def __init__(self, date: str):
+        self.date = date
+        self.warn = "Составлено, но не опубликовано."
+        self.err = "Отсутствует."
+        self.found = "Найдено."
+
+    @property
+    def status(self):
+        raw = requests.get(
+            f"http://rating.ivpek.ru/timetable/timetable/show?gid=324&date={self.date}"
+        ).text
+        s = BeautifulSoup(raw, "lxml")
+        if s.find_all("div", {"class": "msg warning"}):
+            return self.warn
+        if s.find_all("div", {"class": "msg error"}):
+            return self.err
+        return self.found

45c160c9fb72d3e8962aab17e1aded6d804eb9e6
Daniil <dadyarri@gmail.com>
2020-01-10 19:06:10 +0300

    Change Date init mechanism.

--- schedule.py
+++ schedule.py
@@ -6,13 +6,9 @@ from bs4 import BeautifulSoup
 
 
 class Date:
-    @property
-    def today(self):
-        return datetime.today().strftime("%Y-%m-%d")
-
-    @property
-    def tomorrow(self):
-        return datetime.strftime(datetime.now() + timedelta(1), "%Y-%m-%d")
+    def __init__(self):
+        self.today = datetime.today().strftime("%Y-%m-%d")
+        self.tomorrow = datetime.strftime(datetime.now() + timedelta(1), "%Y-%m-%d")
 
 
 class Schedule:

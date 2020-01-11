ea9ed6f564a21837ddab6ca5a9e929d561dc76ad
6a16ec <85ababobkziQVMew6a16ec>
2020-01-11 13:31:11 +0300

    added structure of students` table

---
+++ db/config.py
@@ -0,0 +1,14 @@
+tables = dict()
+
+tables["students"] = {
+    "user_id": "serial PRIMARY KEY",
+    "first_name": "VARCHAR (50) NOT NULL",
+    "surname": "VARCHAR (50) NOT NULL",
+    "patronymic": "VARCHAR (50)",
+    "group_number": "SMALLINT NOT NULL CHECK (group_number > 0)",
+    "subgroup_number": "SMALLINT CHECK (subgroup_number > 0)",
+    "vk_id": "BIGINT NOT NULL CHECK (vk_id > 0)",
+    "tg_id": "BIGINT CHECK (tg_id > 0)",
+    "email": "VARCHAR (355) UNIQUE",
+    "status": "SMALLINT DEFAULT 1 CHECK (1 <= status), CHECK (status <= 5)"
+}

3114eafd821d696f2fc0a43ea76826434173113a
Daniil <dadyarri@gmail.com>
2020-01-11 12:10:41 +0300

    Optimize imports.

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ from bs4 import BeautifulSoup
 from oauth2client.service_account import ServiceAccountCredentials
 from vk_api.bot_longpoll import VkBotEventType
 
-from db.database import Database
+from db import Database
 from students import students
 from vkbotlongpoll import RalphVkBotLongPoll
 

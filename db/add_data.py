e7e9f26b7ca2239275103b12edc28e6fa6f8b2d5
Daniil <dadyarri@gmail.com>
2020-01-12 21:10:05 +0300

    Add script for putting student's data to DB.

---
+++ db/add_data.py
@@ -0,0 +1,13 @@
+import os
+
+from db import Database
+
+db = Database(os.environ["DATABASE_URL"])
+db.connect()
+
+with open("students.txt", "r", encoding="UTF-8") as st:
+    for i in st.readlines():
+        item = ", ".join(i.replace('\n', '').split(':'))
+        a = (f"INSERT into students (vk_id, first_name, surname, group_number, "
+             "subgroup_number, status, is_admin) VALUES ({item})")
+        db.query(a)

01a46e479b75a9d04ab62583eaae4ea58646e88b
Daniil <dadyarri@gmail.com>
2020-01-12 21:07:32 +0300

    Refactor db.py

--- db/__init__.py
+++ db/__init__.py
@@ -11,44 +11,36 @@ class Database:
             "password": url.password,
             "host": url.hostname,
             "port": url.port,
-            "database": url.path[1:]
-            # "dbname": url.path[1:]
+            "database": url.path[1:],
         }
         self.conn, self.cur = None, None
-        self.fetchone, self.fetchall, self.one_query = False, False, False
 
     def create_table(self, name, table: dict):
-        columns = ["%s %s" % (column, type) for column, type in list(table.items())]
-        # print(columns)
+        columns = [f"{column} {_type}" for column, _type in list(table.items())]
         self.query(f"CREATE TABLE {name} ({', '.join(columns)})")
 
     def query(self, query: str, args=None, fetchone=False, fetchall=False):
-        self.fetchone = True if fetchone == True else self.fetchone
-        self.fetchall = True if fetchall == True else self.fetchall
+        commit_func = ["create", "insert", "delete", "update"]
+        result_func = ["select", "returning"]
         self.cur.execute(query, args) if args else self.cur.execute(query)
 
-        commit_func = ["create", "insert", "delete", "update"]
         if any(func in query.lower() for func in commit_func):
-            need_commit = True
-        else:
-            need_commit = False
-        self.commit() if need_commit else None
+            self.commit()
 
-        result_func = ["select", "returning"]
         if any(func in query.lower() for func in result_func):
-            fetch_one, fetch_all = self.fetchone, self.fetchall
-            if not fetch_one and not fetch_all:
-                fetch_all = True
+            if not fetchone and not fetchall:
+                fetchall = True
         else:
-            fetch_one, fetch_all = None, None
+            fetchone, fetchall = None, None
 
-        exec_result = self.cur.fetchone() if fetch_one \
-            else self.cur.fetchall() if fetch_all \
-            else None
+        if fetchone:
+            exec_result = self.cur.fetchone()
+        elif fetchall:
+            exec_result = self.cur.fetchall()
+        else:
+            exec_result = None
 
-        self.close() if self.one_query else None
-
-        if fetch_one or fetch_all:
+        if fetchone or fetchall:
             return exec_result
 
     def commit(self):

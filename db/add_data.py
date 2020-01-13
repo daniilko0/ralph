19ff924ecff2073d6e79ebb059e08b004d8a20d5
Daniil <dadyarri@gmail.com>
2020-01-13 09:07:39 +0300

    Make `add_data` working right.

--- db/add_data.py
+++ db/add_data.py
@@ -1,15 +1,31 @@
 import os
+from pprint import pprint
 
 from db import Database
 
-db = Database(os.environ["DATABASE_URL"])
-db.connect()
 
-with open("students.txt", "r", encoding="UTF-8") as st:
-    for i in st.readlines():
-        item = ", ".join(i.replace("\n", "").split(":"))
-        a = (
-            f"INSERT into students (vk_id, first_name, surname, group_number, "
-            "subgroup_number, status, is_admin) VALUES ({item})"
-        )
-        db.query(a)
+def return_str(itr):
+    return f"'{itr}'"
+
+
+if __name__ == "__main__":
+    db = Database(os.environ["DATABASE_URL"])
+    db.connect()
+
+    cols = [
+        "vk_id",
+        "first_name",
+        "surname",
+        "group_number",
+        "subgroup_number",
+        "status",
+        "is_admin",
+    ]
+
+    with open("students.txt", "r", encoding="UTF-8") as st:
+        for i in st.readlines():
+            item = ", ".join(map(return_str, i.split(":")))
+            db.query(f"INSERT into students ({', '.join(cols)}) VALUES ({item})")
+
+    pprint(db.query("SELECT * FROM students"))
+    db.close()

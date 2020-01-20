e380b416db0536b12c344c90ca38fd5792c69a1b
Daniil <dadyarri@gmail.com>
2020-01-20 19:11:39 +0300

    Add `get_last_names_letters` method.

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,20 @@
+from itertools import groupby
+
 from database.base import Base
 
 
 class Database(Base):
-    pass
+    """
+        Класс для методов работы с БД, выполняющих конечную цель
+    """
+
+    def get_last_names_letters(self):
+        """
+        Получает из базы данных все уникальные первые буквы фамилий
+        """
+        p = self.query("SELECT second_name FROM users_info")
+        names = []
+        for i, v in enumerate(p):
+            names.append(v[0][0])
+        names = [el for el, _ in groupby(names)]
+        return names

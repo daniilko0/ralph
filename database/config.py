eff1bd5b8526ee0647943253f4cb686e120b4af6
Daniil <dadyarri@gmail.com>
2020-02-09 18:52:59 +0300

    Update database structure in config.

--- database/config.py
+++ database/config.py
@@ -1,11 +1,36 @@
 tables = dict()
 
+tables["calls"] = {"seesion_id": "integer", "ids": "varchar(400)"}
+
+tables["mailing_mgmt"] = {
+    "session_id": "integer",
+    "mailing": "varchar(50)",
+    "m_text": "varchar(1000)",
+}
+
+tables["mailings"] = {
+    "mailing_id": "serial integer",
+    "mailing_name": "varchar(70)",
+    "mailing_slug": "varchar(30)",
+}
+
+tables["sessions"] = {
+    "id": "serial integer",
+    "vk_id": "integer",
+    "state": "varchar(35)",
+    "conversation": "integer default 2000000001",
+}
+
+tables["texts"] = {
+    "session_id": "integer",
+    "text": "varchar(1200)",
+}
+
 tables["users"] = {
     "id": "serial PRIMARY KEY",
     "vk_id": "BIGINT NOT NULL CHECK (vk_id > 0) UNIQUE",
     "tg_id": "BIGINT CHECK (tg_id > 0) UNIQUE",
 }
-
 
 tables["users_info"] = {
     "user_id": "INTEGER NOT NULL PRIMARY KEY",
@@ -16,21 +41,9 @@ tables["users_info"] = {
     "academic_status": "SMALLINT NOT NULL",
 }
 
-tables["sessions"] = {
-    "id": "serial",
-    "user_id": "INTEGER NOT NULL",
-    "state": "INTEGER DEFAULT 0",
-    "social": "SMALLINT DEFAULT 0",
-    "PRIMARY KEY": "(user_id, social)",
-}
-
-tables["users_list"] = {
-    "session_id": "INTEGER NOT NULL",
-    "user_id": "INTEGER NOT NULL",
-    "UNIQUE": "(session_id, user_id)",
-}
-
-tables["texts"] = {
-    "session_id": "INTEGER NOT NULL PRIMARY KEY",
-    "text": "VARCHAR (255) NOT NULL",
+tables["vk_subscriptions"] = {
+    "user_id": "serial integer",
+    "common": "smallint default 1",
+    "schedule": "smallint default 1",
+    "updates": "smallint default 0",
 }

e1c8a00d9e1285b7839b7339117ab5b51c6d9ceb
Daniil <dadyarri@gmail.com>
2020-02-18 10:54:55 +0300

    Fix scripts.

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -9,6 +9,10 @@ echo "Folder with project deleted."
 
 # Create new virtual environment
 mkdir /home/ubuntu/projects/ralph
+
+# Move downloaded bundle to project's directory
+mv {$PWD}/* /home/ubuntu/projects/ralph
+
 cd /home/ubuntu/projects/ralph
 echo "Setting up virtual environment..."
 /home/ubuntu/.python/bin//python3.8 -m venv venv_ralph
@@ -16,11 +20,11 @@ echo "Setting up virtual environment finished."
 
 # Installing requirements
 . /home/ubuntu/projects/ralph/venv_ralph/bin/activate
-pip install -r requirements.txt
+pip install -r /home/ubuntu/projects/ralph/requirements.txt
 
 
 # Starting new session
 tmux new-session -s ralph -d
-tmux send-keys -t ralph:1 " cd projects/ralph && cls && python main.py" Enter
+tmux send-keys -t ralph:1 " cd /home/ubuntu/projects/ralph && cls && python main.py" Enter
 tmux new-window
-tmux send-keys -t ralph:2 " cd projects/ralph && cls && python scheduler.py" Enter
+tmux send-keys -t ralph:2 " cd /home/ubuntu/projects/ralph && cls && python scheduler.py" Enter

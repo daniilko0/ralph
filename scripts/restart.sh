b665489bff50c8d500a484a5d76d1ee6e146b386
Daniil <dadyarri@gmail.com>
2020-02-18 11:19:45 +0300

    Update scripts.

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,6 +2,6 @@
 
 # Starting new session
 tmux new-session -s ralph -d
-tmux send-keys -t ralph:1 " cd /home/ubuntu/projects/ralph && cls && python main.py" Enter
+tmux send-keys -t ralph:0 " cd /home/ubuntu/projects/ralph && cls && python main.py" Enter
 tmux new-window
-tmux send-keys -t ralph:2 " cd /home/ubuntu/projects/ralph && cls && python scheduler.py" Enter
+tmux send-keys -t ralph:1 " cd /home/ubuntu/projects/ralph && cls && python scheduler.py" Enter

e5ba10d735c63537b949bbdf1582280f1daf679a
Daniil <dadyarri@gmail.com>
2020-02-18 10:21:53 +0300

    Edit config AWS.

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# Stop tmux's session
+tmux kill-server
+
+# Delete all ralph's contain
+rm -rf projects/ralph
+echo "Folder with project deleted."
+
+# Create new virtual environment
+mkdir projects/ralph
+cd projects/ralph
+echo "Setting up virtual environment..."
+python3.8 -m venv venv_ralph
+echo "Setting up virtual environment finished."
+
 # Installing requirements
 . venv_ralph/bin/activate
 pip install -r requirements.txt

1878d49e2296ed4333631580e2a190b91eef65dc
Daniil <dadyarri@gmail.com>
2020-02-22 15:36:48 +0300

    Update docs config.

--- docs/conf.py
+++ docs/conf.py
@@ -63,10 +63,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = "sphinx_rtd_theme"
 
-html_theme_options = {
-    'show_powered_by': False,
-}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

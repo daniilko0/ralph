23a2301b9e947a2e65540ac1188901ece13d5cb8
Daniil <dadyarri@gmail.com>
2020-02-24 10:28:19 +0300

    Use napoleon as docstring parser.

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ with open("../VERSION.txt", "r") as f:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc"]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]

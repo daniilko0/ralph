e78c9cb39dc1b2e27cee9ced1489ee5d833dc519
Daniil <dadyarri@gmail.com>
2020-02-24 11:03:41 +0300

    Add showind TODOs in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ with open("../VERSION.txt", "r") as f:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx_rtd_theme", "sphinx.ext.napoleon"]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.napoleon", "sphinx.ext.todo"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -65,5 +65,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+todo_include_todos = True
 
 master_doc = "index"

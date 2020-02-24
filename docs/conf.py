b3ce0c64f7709f06b8884d0703064dc871db9721
Daniil <dadyarri@gmail.com>
2020-02-24 12:46:02 +0300

    Add using of custom css.

--- docs/conf.py
+++ docs/conf.py
@@ -66,6 +66,10 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = [
+    "css/custom.css"
+]
+
 todo_include_todos = True
 
 master_doc = "index"

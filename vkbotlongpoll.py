fa0e6fcf3c4cfb0c62287caca9ba7a1d66e741cc
Daniil Golubev <dadyarri@gmail.com>
2020-02-28 12:56:09 +0300

    Add descriptioning comment to vkbotlongpoll.py

--- vkbotlongpoll.py
+++ vkbotlongpoll.py
@@ -1,5 +1,7 @@
-from vk_api.bot_longpoll import VkBotLongPoll
+# Эта хрень нужна, чтобы бот не останавливался каждый день примерно в три часа ночи (ВК перезапускает свои сервера)
+# Подробнее здесь: https://github.com/python273/vk_api/issues/144#issuecomment-404023710
 
+from vk_api.bot_longpoll import VkBotLongPoll
 
 class RalphVkBotLongPoll(VkBotLongPoll):
     def listen(self):

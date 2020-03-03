13f09e86cb89c487b7372f6555dfbfde93211655
Daniil <dadyarri@gmail.com>
2020-03-03 18:27:49 +0300

    Add try-block for a case if logger's config wasn't found.

--- logger/__init__.py
+++ logger/__init__.py
@@ -7,8 +7,12 @@ import yaml
 
 class Logger:
     def __init__(self):
-        with open("logger.yml", "r") as f:
-            self.config = yaml.safe_load(f.read())
+        try:
+            with open("logger.yml", "r") as f:
+                self.config = yaml.safe_load(f.read())
+        except FileNotFoundError:
+            with open("logger/logger.yml", "r") as f:
+                self.config = yaml.safe_load(f.read())
         pprint(self.config)
         self.log = logging.getLogger("tg")
         logging.config.dictConfig(config=self.config)

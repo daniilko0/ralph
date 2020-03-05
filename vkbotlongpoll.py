aa88cedb56d1fe1cf44c02cc3793ff7a364af369
Daniil <dadyarri@gmail.com>
2020-03-05 15:12:00 +0300

    Add new logger to longpoll listener.

--- vkbotlongpoll.py
+++ vkbotlongpoll.py
@@ -2,13 +2,18 @@
 # Подробнее здесь: https://github.com/python273/vk_api/issues/144#issuecomment-404023710
 
 from vk_api.bot_longpoll import VkBotLongPoll
+from logger import init_logger
 
 
 class RalphVkBotLongPoll(VkBotLongPoll):
+    def __init__(self, vk, group_id, wait=25):
+        super().__init__(vk, group_id, wait)
+        self.log = init_logger()
+
     def listen(self):
         while True:
             try:
                 for event in self.check():
                     yield event
             except Exception as err:
-                print(f"Error: {err}.")
+                self.log.info(f"Error: {err}.")

67ce9783596639e545e84dda8c1949421b940a28
Daniil <dadyarri@gmail.com>
2020-03-10 19:46:22 +0300

    Remove Amazon scripts.

--- scripts/remove.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Stop tmux's session
-tmux kill-session
-
-# Delete all ralph's contain except .envrc
-sudo rm -rf `ls | grep -v ".envrc"`
-echo "Folder with project deleted."
--- scripts/restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Starting new session
-tmux new-session -s ralph -d
-tmux send-keys -t ralph:0 " cd /home/ubuntu/projects/ralph && cls && python main.py" Enter
-tmux new-window
-tmux send-keys -t ralph:1 " cd /home/ubuntu/projects/ralph && cls && python scheduler.py" Enter

88ea35d344260cd4fbe9f4f9d22ebc1042171058
Daniil <dadyarri@gmail.com>
2020-03-24 09:41:42 +0300

    Polish UI.

--- keyboard/__init__.py
+++ keyboard/__init__.py
@@ -140,10 +140,10 @@ class Keyboards:
         kb.add_button(label="📈 Доход", payload={"button": "add_donate"})
         kb.add_button(label="📉 Расход", payload={"button": "add_expense"})
         kb.add_line()
-        kb.add_button(label="Должники", payload={"button": "debtors"})
-        kb.add_button(label="Настройки", payload={"button": "fin_prefs"})
+        kb.add_button(label="📢 Должники", payload={"button": "debtors"})
+        kb.add_button(label="⚙ Настройки", payload={"button": "fin_prefs"})
         kb.add_line()
-        kb.add_button(label="Назад", color="primary", payload={"button": "finances"})
+        kb.add_button(label="👈🏻 Назад", color="primary", payload={"button": "finances"})
         return kb.get_keyboard()
 
     @staticmethod
